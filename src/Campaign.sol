b88a60e42276bfae96d9fb18460a54735491f2dd
soyboy <sbvegan@protonmail.com>
2022-09-18 12:37:27 -0700

    adding todo

--- src/Campaign.sol
+++ src/Campaign.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.13;
+// todo: make camapaign ownable
+import "openzeppelin/access/Ownable.sol";
 
-contract Campaign {
+contract Campaign is Ownable {
     uint256 public immutable id;
 
-    constructor(uint256 id_) {
+    constructor(uint256 id_ /**, address owner */) {
         id = id_;
+        // transferOwnership(owner);
     }
 }
